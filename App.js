28e71bd5abac233620f19fbf462599b48742aca1
Cristopher <cristopher1718@yahoo.es>
2023-01-18 18:31:49 -0600

    Add Button and  two avatars

--- App.js
+++ App.js
@@ -1,12 +1,48 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { NativeBaseProvider, Box } from "native-base";
+import { NativeBaseProvider, Box, Button, Avatar, VStack } from "native-base";
 
 export default function App() {
   return (
     <NativeBaseProvider>
       <Box>Hello world</Box>
+      <Button
+        colorScheme="primary"
+        onPress={() => {
+          console.log('chino kyc')
+        }}
+
+      >
+        hola que hace
+      </Button>
+
+      <VStack space="20"
+        marginY={"auto"}
+        alignItems={{
+          base: "center",
+          md: "flex-start"
+        }}
+      >
+
+        <Avatar
+          source={require('./assets/1.jpg')}
+        >
+          Monoz
+        </Avatar>
+
+        <Avatar
+          source={{
+            uri: "https://pbs.twimg.com/profile_images/1369921787568422915/hoyvrUpc_400x400.jpg"
+          }}
+
+        >
+          SS
+        </Avatar>
+      </VStack>
+
+
+
     </NativeBaseProvider>
   );
 }
