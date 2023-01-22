c468b623bce21f1df16f80675c0fe3be0ab477f8
Chiyen Venegas <chi@marandino.dev>
2023-01-22 12:06:42 -0600

    add(nb theme): color scheme

--- App.js
+++ App.js
@@ -1,18 +1,37 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NativeBaseProvider } from "native-base";
+import { NativeBaseProvider, extendTheme } from "native-base";
 import HomeScreen from "./screens/HomeScreen";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const theme = extendTheme({
+    colors: {
+      primary: {
+        400: "#f98120",
+        500: "#f98120",
+        600: "#f98120",
+      },
+      secondary: {
+        400: "#3b82f6",
+        500: "#3b82f6",
+        600: "#3b82f6",
+      },
+    },
+    config: {
+      // Changing initialColorMode to 'dark'
+      // initialColorMode: "dark",
+    },
+  });
+
   return (
-  <NativeBaseProvider >
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  </NativeBaseProvider>
+    <NativeBaseProvider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
